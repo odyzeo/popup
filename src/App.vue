f2e6d3ec560d11aa38a6e98ce854158e4fc76b77
Tomáš Zamba <tomas.zamba@odyzeo.com>
2021-06-22 17:11:47 +0200

    Add popup close wrapper for default slot w/ example

--- src/App.vue
+++ src/App.vue
@@ -192,6 +192,9 @@
                         </button>
                     </div>
                 </template>
+                <template #close>
+                    CLOSE
+                </template>
             </popup>
         </div>
     </div>
